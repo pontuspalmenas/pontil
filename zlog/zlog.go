0bde0a17ab0f3000b003caa7ed4824ad6fede2c9
Pontus Palmenäs <pontus.palmenas@konghq.com>
2026-04-01 10:38:51 +0200

    chore: refactor zlog

--- zlog/zlog.go
+++ zlog/zlog.go
@@ -1,41 +1,82 @@
 package zlog
 
 import (
-	"cmp"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
-func Trace(s string) {
-	if cmp.Or(os.Getenv("LOG_LEVEL"), "default") == "trace" {
-		writeLog("[TRACE] " + s + "\n")
+const (
+	levelDebug = iota
+	levelInfo
+	levelWarn
+	levelError
+)
+
+func logLevel() int {
+	switch strings.ToLower(os.Getenv("LOG_LEVEL")) {
+	case "debug":
+		return levelDebug
+	case "info":
+		return levelInfo
+	case "warn":
+		return levelWarn
+	case "error":
+		return levelError
+	default:
+		return levelInfo
 	}
 }
 
-func Debug(s string) {
-	writeLog("[DEBUG] " + s + "\n")
+func Debug(msg string) {
+	if logLevel() <= levelDebug {
+		writeLog("[debug] " + msg + "\n")
+	}
 }
 
 func Debugf(format string, a ...any) {
-	Debug(fmt.Sprintf(format, a...))
+	if logLevel() <= levelDebug {
+		writeLog("[debug] " + fmt.Sprintf(format, a...) + "\n")
+	}
 }
 
-func Info(s string) {
-	writeLog("[INFO] " + s + "\n")
+func Info(msg string) {
+	if logLevel() <= levelInfo {
+		writeLog("[info] " + msg + "\n")
+	}
 }
 
 func Infof(format string, a ...any) {
-	Info(fmt.Sprintf(format, a...))
+	if logLevel() <= levelInfo {
+		writeLog("[info] " + fmt.Sprintf(format, a...) + "\n")
+	}
 }
 
-func Error(s string) {
-	writeLog("[ERROR] " + s + "\n")
+func Warn(msg string) {
+	if logLevel() <= levelWarn {
+		writeLog("[warn] " + msg + "\n")
+	}
+}
+
+func Warnf(format string, a ...any) {
+	if logLevel() <= levelWarn {
+		writeLog("[warn] " + fmt.Sprintf(format, a...) + "\n")
+	}
+}
+
+func Error(msg string) {
+	if logLevel() <= levelError {
+		writeLog("[error] " + msg + "\n")
+	}
 }
 
 func Errorf(format string, a ...any) {
-	Error(fmt.Sprintf(format, a...))
+	if logLevel() <= levelError {
+		writeLog("[error] " + fmt.Sprintf(format, a...) + "\n")
+	}
 }
 
 func writeLog(s string) {
-	fmt.Print(s)
+	fmt.Printf("%s %s", time.Now().Format(time.RFC3339), s)
 }
